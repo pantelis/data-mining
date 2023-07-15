a7e54b47b642f0a5381abc9cd557681adc8c7fa2
Pantelis Monogioudis <pantelis.monogioudis@aegean.ai>
2023-07-15 11:38:08 +0000

    updated workflow and _config.yml

--- data_mining/conf.py
+++ data_mining/conf.py
@@ -5,18 +5,20 @@
 ###############################################################################
 author = 'Pantelis Monogioudis, Ph.D'
 bibtex_bibfiles = ['ai.bib']
+bibtex_reference_style = 'author_year'
 comments_config = {'hypothesis': False, 'utterances': False}
 copyright = '2023'
 exclude_patterns = ['**.ipynb_checkpoints', '.DS_Store', 'Thumbs.db', '_build']
-extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_design', 'sphinx_book_theme', 'sphinxcontrib.youtube', 'sphinxcontrib.bibtex', 'sphinx_jupyterbook_latex']
+extensions = ['sphinx_togglebutton', 'sphinx_copybutton', 'myst_nb', 'jupyter_book', 'sphinx_thebe', 'sphinx_comments', 'sphinx_external_toc', 'sphinx.ext.intersphinx', 'sphinx_design', 'sphinx_book_theme', 'sphinxcontrib.youtube', 'sphinxcontrib.inkscapeconverter', 'sphinxcontrib.bibtex', 'sphinx_jupyterbook_latex']
 external_toc_exclude_missing = True
 external_toc_path = '_toc.yml'
 html_baseurl = 'https://pantelis.github.io/data-mining'
 html_favicon = './logo.ico'
 html_logo = './logo.png'
+html_sidebars = {'data_mining/*': []}
 html_sourcelink_suffix = ''
 html_theme = 'sphinx_book_theme'
-html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': 'https://mybinder.org', 'jupyterhub_url': '', 'thebe': True, 'colab_url': 'https://colab.research.google.com'}, 'path_to_docs': 'data_mining', 'repository_url': 'https://github.com/pantelis/data-mining', 'repository_branch': 'master', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '', 'analytics': {'google_analytics_id': ''}, 'use_repository_button': True, 'use_edit_page_button': True, 'use_issues_button': True, 'use_download_button': True}
+html_theme_options = {'search_bar_text': 'Search this book...', 'launch_buttons': {'notebook_interface': 'classic', 'binderhub_url': 'https://mybinder.org', 'jupyterhub_url': '', 'thebe': True, 'colab_url': 'https://colab.research.google.com'}, 'path_to_docs': 'data_mining', 'repository_url': 'https://github.com/pantelis/data-mining', 'repository_branch': 'master', 'extra_footer': '', 'home_page_in_toc': True, 'announcement': '', 'analytics': {'google_analytics_id': ''}, 'use_repository_button': True, 'use_edit_page_button': True, 'use_issues_button': True, 'use_download_button': True, 'show_navbar_depth': 0, 'intersphinx_mapping': None, 'python': ['https://docs.python.org/3/', None], 'numpy': ['https://numpy.org/doc/stable/', None], 'pandas': ['https://pandas.pydata.org/docs/', None], 'scikit-learn': ['https://scikit-learn.org/stable/', None], 'torch': ['https://pytorch.org/docs/master/', None], 'tf2': ['https://pytorch.org/docs/master/', None]}
 html_title = 'Data Mining'
 latex_elements = {'preamble': '\\newcommand\\N{\\mathbb{N}}\n\\newcommand\\floor[1]{\\lfloor#1\\rfloor}\n\\newcommand{\\bmat}{\\left[\\begin{array}}\n\\newcommand{\\emat}{\\end{array}\\right]}\n'}
 latex_engine = 'xelatex'
